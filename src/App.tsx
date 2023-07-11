6eecbb71f5687b68a6b24dc53c298347a359488a
joshnwosu <joshuanwosu078@gmail.com>
2023-07-11 03:24:39 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import {
-  MantineProvider,
-  SegmentedControl,
-  Text,
-  createStyles,
-} from "@mantine/core";
+import { SegmentedControl, Text, createStyles } from "@mantine/core";
 import { useEffect } from "react";
 
 const useStyle = createStyles((theme) => ({
   title: {
     fontSize: 20,
     color: theme.colors[theme.primaryColor][6],
+    fontWeight: "lighter",
   },
 }));
 
 export default function App() {
   const { classes, theme } = useStyle();
+
   useEffect(() => {
-    alert(theme.colorScheme);
+    console.log("Color Scheme: ", theme.colorScheme);
   }, []);
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <>
       <Text className={classes.title}>Welcome to Mantine!</Text>
       <SegmentedControl
         data={["All", "Iyana Ipaja", "Ajah", "Lekki", "Ikoyi", "Egbeda"]}
       />
-    </MantineProvider>
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { MantineProvider } from "@mantine/core";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+      }}
+    >
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
