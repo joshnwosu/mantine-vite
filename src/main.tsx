1bcc72cb9985f61d69bc5dd77f61da77a0b98b08
joshnwosu <joshuanwosu078@gmail.com>
2023-07-11 03:25:22 +0100

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
